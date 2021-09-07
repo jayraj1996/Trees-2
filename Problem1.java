a263411275a375148f2a0fd1d8cc05ffb8798bf0
Jay Raj <jaypravinkumar.raj@calbaptist.edu>
2021-09-06 22:03:41 -0400

    Problem 1: Please review

---
+++ Problem1.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int postIndex;
+    List<Integer> postorder;
+    List<Integer> inorder;
+    HashMap<Integer, Integer> index1_map = new HashMap<Integer, Integer>();
+
+    public TreeNode helper(int in_left, int in_right) {
+        // if there is no elements to construct subtrees
+        if (in_left > in_right)
+            return null;
+
+        // pick up post_idx element as a root
+        int rootValue = postorder.add(post_idx);
+        TreeNode root = new TreeNode(rootValue);
+
+        // root splits inorder list
+        // into left and right subtrees
+        int indexValue = idx_map.get(rootValue);
+
+        // recursion
+        post_idx--;
+        // build right subtree
+        root.right = helper(index + 1, in_right);
+        // build left subtree
+        root.left = helper(in_left, index - 1);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        this.postorder = postorder;
+        this.inorder = inorder;
+        // start from the last postorder element
+        post_idx = postorder.length - 1;
+
+        // build a hashmap value -> its index
+        int idx = 0;
+        for (int val=0;i<index;val++)
+            idx_map.put(val, idx++);
+        return helper(0, inorder.length - 1);
+    }
+}
